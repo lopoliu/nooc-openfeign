16fea2628ad78bc8ea5350185022879258dec4d8
lopo <lopo971020@icloud.com>
2023-11-23 21:50:32 +0800

    openfeign 学习

--- README.md
+++ README.md
@@ -1,14 +1,56 @@
-# nooc-nacos
-这是一个spring cloud nacos学习项目
+# nooc-openfeign
+这是一个nacos+openfeign学习项目
 
-记录学习[nacos](https://nacos.io/zh-cn/)的笔记代码
+记录学习openfeign的笔记代码
 
-## 安装nacos服务
-下载nacos服务: [Github下载](https://github.com/alibaba/nacos/releases)
 
-- 启动nacos  
-```sh startup.sh -m standalone```
-- 访问nacos
-  - 控制台: http://localhost:8848/nacos/#
-  - nacos默认端口启动在8848
-  - nacos默认账户名*nacos*、默认密码*macos*
+添加openfeign依赖
+```xml
+<dependency>
+    <groupId>org.springframework.cloud</groupId>
+    <artifactId>spring-cloud-starter-openfeign</artifactId>
+</dependency>
+```
+
+添加loadbalancer依赖
+```xml
+<dependency>
+    <groupId>org.springframework.cloud</groupId>
+    <artifactId>spring-cloud-starter-loadbalancer</artifactId>
+</dependency>
+```
+配置client
+```java
+@FeignClient(value = "userService")
+public interface UserClient {
+    @GetMapping("/user/{userId}")
+    User selectUserById(@PathVariable("userId") Long userId);
+}
+```
+
+调用远程服务
+```java
+@Service
+public class OrderServiceImpl implements OrderService {
+    private OrderMapper orderMapper;
+
+    private UserClient userClient;
+    @Autowired
+    public void setOrderMapper(OrderMapper orderMapper) {
+        this.orderMapper = orderMapper;
+    }
+
+    @Autowired
+    public void setUserClient(UserClient userClient) {
+        this.userClient = userClient;
+    }
+
+    @Override
+    public Order getById(Integer orderId) {
+        Order order = orderMapper.selectById(orderId);
+        User user = userClient.selectUserById(order.getUserId());
+        order.setUser(user);
+        return order;
+    }
+}
+```
--- order-service/src/main/java/com/lopo/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/com/lopo/service/impl/OrderServiceImpl.java
@@ -1,47 +1,32 @@
 package com.lopo.service.impl;
 
+import com.lopo.clients.UserClient;
 import com.lopo.domain.Order;
 import com.lopo.domain.User;
 import com.lopo.mapper.OrderMapper;
 import com.lopo.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
-import java.util.Random;
 
 @Service
 public class OrderServiceImpl implements OrderService {
     private OrderMapper orderMapper;
-    private RestTemplate restTemplate;
-    private DiscoveryClient discoveryClient;
+
+    private UserClient userClient;
     @Autowired
     public void setOrderMapper(OrderMapper orderMapper) {
         this.orderMapper = orderMapper;
     }
 
     @Autowired
-    public void setRestTemplate(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
-
-    @Autowired
-    public void setDiscoveryClient(DiscoveryClient discoveryClient) {
-        this.discoveryClient = discoveryClient;
+    public void setUserClient(UserClient userClient) {
+        this.userClient = userClient;
     }
 
     @Override
     public Order getById(Integer orderId) {
         Order order = orderMapper.selectById(orderId);
-
-        List<ServiceInstance> instances = discoveryClient.getInstances("userService");
-        Random random = new Random();
-        ServiceInstance serviceInstance = instances.get(random.nextInt(instances.size()));
-        String url = serviceInstance.getUri() +"/user/" + order.getUserId();
-        User user = restTemplate.getForObject(url, User.class);
+        User user = userClient.selectUserById(order.getUserId());
         order.setUser(user);
         return order;
     }
